2e772fad67badaf4f6646208d017ee033cd8d098
CHAUKIENLUONG <luong211003@gmail.com>
2024-04-28 00:56:33 +0700

    init project

---
+++ B-Talk project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B_Talk_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
